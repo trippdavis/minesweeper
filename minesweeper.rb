61d389ca93fec40bfccc0bfbb0cd8b6726e7acfd
Kenneth Davis <trippdavis3@gmail.com>
2015-02-23 16:44:12 -0800

    Refactor code and add save/load feature

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,9 +1,104 @@
 require 'byebug'
+require 'yaml'
 
+
+MOVES = [[-1, 0],[-1, 1],[0, 1], [1, 1], [1,0], [1,-1], [0,-1],[-1,-1]]
 class Minesweeper
+  attr_reader :board
+
   def initialize
     @board = Board.new
   end
+
+  def play
+    #player picks
+    #game checks spot
+    #decide if winner
+    until won?
+      board.display
+      pick = get_move
+      unless pick
+        puts "Invalid pick"
+        next
+      end
+      tile_x = pick[1].to_i
+      tile_y = pick[2].to_i
+      picked_tile = @board.board[tile_x][tile_y]
+
+      case
+      when picked_tile.flagged?
+        next
+      when pick[0] == "f"
+        picked_tile.state = "F"
+        next
+      when picked_tile.type == "B" && picked_tile.state == "*"
+        puts 'You lose!'
+        return
+      when picked_tile.bomb_count > 0
+        picked_tile.reveal
+      when picked_tile.bomb_count == 0
+        reveal_bomb_count(picked_tile)
+      end
+
+
+    end
+
+    puts "You won!"
+  end
+
+  def won?
+    board.board.each do |row|
+      row.each do |space|
+          return false if space.type == "S" && space.state == "*"
+      end
+    end
+
+    true
+  end
+
+  def reveal_bomb_count(picked_tile)
+
+    neighbors = [picked_tile]
+    until neighbors.empty?
+      # new_neighbors = []
+      this_tile = neighbors.shift
+      next unless this_tile.state == "*"
+      this_tile.reveal
+      MOVES.each do |move|
+        move_x = this_tile.pos[0] + move[0]
+        move_y = this_tile.pos[1] + move[1]
+        unless @board.out_of_bounds?([move_x, move_y])
+          neighbor_tile = @board.board[move_x][move_y]
+          if this_tile.bomb_count == 0
+            neighbors << neighbor_tile
+          end
+        end
+      end
+    end
+  end
+
+  def get_move
+    puts "What tile do you want to pick? (type s to save game)"
+    pick = gets.chomp.split(',') # ex. [1,2]
+    if pick[0] == "s"
+      save
+      abort
+    end
+    return false unless pick.count == 3
+    pick
+  end
+
+  def save
+    File.open('minesweeper.yml', 'w') do |f|
+      f.puts self.to_yaml
+    end
+  end
+
+  def self.load
+    game = YAML::load_file('minesweeper.yml')
+    game.play
+  end
+
 end
 
 class Board
@@ -11,10 +106,11 @@ class Board
 
   def initialize
     @board = Array.new(9) { Array.new(9) }
+    populate_board
   end
 
   def populate_board
-    tiles = ["B"] * 9 + ["S"] * 72
+    tiles = ["B"] * 3 + ["S"] * 78
     tiles.shuffle!
     @board.each_with_index do |row, row_i|
       row.each_with_index do |col, col_i|
@@ -56,86 +152,6 @@ class Board
     return nil
   end
 
-  def get_move
-    puts "What tile do you want to pick?"
-    pick = gets.chomp.split(',') # ex. [1,2]
-  end
-
-
-
-  def play
-    #player picks
-    #game checks spot
-    #decide if winner
-    until won?
-      display
-      pick = get_move
-      tile_x = pick[1].to_i
-      tile_y = pick[2].to_i
-      picked_tile = @board[tile_x][tile_y]
-
-      next if picked_tile.flagged?
-      if pick[0] == "f"
-        picked_tile.state = "F"
-        next
-      end
-
-      if picked_tile.type == "B" && picked_tile.state == "*"
-        #end of game
-        puts 'You lose!'
-        return
-      end
-
-      if picked_tile.bomb_count > 0
-        picked_tile.reveal
-      end
-
-      if picked_tile.bomb_count == 0
-        reveal_bomb_count(picked_tile)
-      end
-    end
-
-    puts "You won!"
-  end
-
-
-
-
-
-  def won?
-    @board.each do |row|
-      row.each do |space|
-          return false if space.type == "S" && space.state == "*"
-      end
-    end
-
-    true
-  end
-
-  def reveal_bomb_count(picked_tile)
-    moves = [[-1, 0],[-1, 1],[0, 1], [1, 1], [1,0], [1,-1], [0,-1],[-1,-1]]
-    neighbors = [picked_tile]
-    until neighbors.empty?
-      new_neighbors = []
-
-      neighbors.each do |tile|
-        next unless tile.state == "*"
-        tile.reveal
-        moves.each do |move|
-          move_x = tile.pos[0] + move[0]
-          move_y = tile.pos[1] + move[1]
-          unless out_of_bounds?([move_x, move_y])
-            neighbor_tile = @board[move_x][move_y]
-            if tile.bomb_count == 0
-              new_neighbors << neighbor_tile
-            end
-          end
-        end
-      end
-
-      neighbors = new_neighbors
-    end
-  end
 end
 
 class Tile
@@ -151,8 +167,8 @@ class Tile
   end
 
   def populate_bombs
-    moves = [[-1, 0],[-1, 1],[0, 1], [1, 1], [1,0], [1,-1], [0,-1],[-1,-1]]
-    moves.each do |move|
+
+    MOVES.each do |move|
       if (pos[0] + move[0]) >= 0 && (pos[0] + move[0]) < 9 &&
          (pos[1] + move[1]) >= 0 && (pos[1] + move[1]) < 9
 
